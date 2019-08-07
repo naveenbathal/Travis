aa448e1c5d4a5fef3ebc988d8c7227990e8eff8e
Naveen bathal <naveenbathal@Naveens-MacBook-Air.local>
2019-08-07 19:05:50 -0400

    Yo make it right and check travis

--- test/serious-calculations-test.js
+++ test/serious-calculations-test.js
@@ -5,7 +5,7 @@ var assert = require('assert'),
 vows.describe('serious-calculations').addBatch({
   'when performing serious calculations': {
     topic: function() {
-      return seriousCalculations.performSeriousCalculations(434);
+      return seriousCalculations.performSeriousCalculations(4);
     },
     'result should be valid': function (topic) {
       assert.isNumber(topic);
